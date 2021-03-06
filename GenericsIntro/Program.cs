3d249f806caefa8dd01b84af3e282de7c2e291b9
TUĞÇE YAZICI <l2319050@std.yildiz.edu.tr>
2021-03-06 16:29:40 +0300

    06.03.2021

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList isimler = new MyList();
+            isimler.Add("Engin");
+
+        }
+    }
+}
